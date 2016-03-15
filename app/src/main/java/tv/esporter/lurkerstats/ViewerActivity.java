ae7fe743b93877f50a38080d6680021f38feb426
Sergey Demyanov <s.demyanov@gmail.com>
2016-03-14 21:54:15 -0400

    more stuff for #4

--- app/src/main/java/tv/esporter/lurkerstats/ViewerActivity.java
+++ app/src/main/java/tv/esporter/lurkerstats/ViewerActivity.java
@@ -64,6 +64,7 @@ public class ViewerActivity extends AppCompatActivity
 
         final ActionBar ab = getSupportActionBar();
 //        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayShowHomeEnabled(true);
         ab.setDisplayHomeAsUpEnabled(getIntent().hasExtra(EXTRA_USERNAME));
 
         mDataServiceHelper = new DataServiceHelper(new Handler(), this);
@@ -116,12 +117,15 @@ public class ViewerActivity extends AppCompatActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                return true;
+            case android.R.id.home:
+                this.finish();
+                return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
